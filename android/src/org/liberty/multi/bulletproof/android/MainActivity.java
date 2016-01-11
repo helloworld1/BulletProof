1a55ebbd2dcb2e993d85551ecb14aedbbbfe97d4
Howard Liberty <liberty@anymemo.org>
2016-01-11 11:35:05 -0800

    Make the immersive mode sticky

--- android/src/org/liberty/multi/bulletproof/android/MainActivity.java
+++ android/src/org/liberty/multi/bulletproof/android/MainActivity.java
@@ -49,16 +49,6 @@ public class MainActivity extends AndroidApplication implements ScoreBoardResolv
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Enable immersive mode
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        
         // Setup application
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
 
@@ -156,6 +146,23 @@ public class MainActivity extends AndroidApplication implements ScoreBoardResolv
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         gameHelper.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Enable immersive mode
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        }
     }
 
     private void unlockAchievements(float time) {
